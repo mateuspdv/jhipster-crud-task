5e4cb4304c3b35a363a37ee24e1309cfdd213126
Mateus Padovan Pereira <mateuspadovanpereira@hotmail.com>
2023-08-07 22:46:26 -0300

    CRUD da entidade Categoria implementado na interface

--- frontend/src/main/webapp/app/entities/entity-routing.module.ts
+++ frontend/src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,11 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'category',
+        data: { pageTitle: 'jhipsterCrudTaskApp.category.home.title' },
+        loadChildren: () => import('./category/category.routes'),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
